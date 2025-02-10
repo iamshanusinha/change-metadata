8581818174d0826d2ca26056603d264bf16bc9f3
Shanu sinha <118159555+iamshanusinha@users.noreply.github.com>
2025-02-11 00:32:17 +0530

    uploading files

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from changemetadata import create_app
 
-app=create_app()
+app = create_app()
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
